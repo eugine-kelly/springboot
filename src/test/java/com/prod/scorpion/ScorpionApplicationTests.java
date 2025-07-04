e7be767356f3affbae708a2080b9f92aa081dbb3
eugine <kellyeugine583@gmail.com>
2025-07-04 10:00:01 +0300

    Fixed test

--- src/test/java/com/prod/scorpion/ScorpionApplicationTests.java
+++ src/test/java/com/prod/scorpion/ScorpionApplicationTests.java
@@ -1,15 +1,13 @@
 package com.prod.scorpion;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-@ActiveProfiles("test")
+@TestPropertySource(properties = {
+        "spring.datasource.url=jdbc:h2:mem:testdb",
+        "spring.datasource.driver-class-name=org.h2.Driver"
+})
 class ScorpionApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
+    // ...
 }
